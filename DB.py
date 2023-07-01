af66b4f9e970fda7235335a3329b557a781180c8
sunnym709 <125447994+sunnym709@users.noreply.github.com>
2023-07-01 16:21:52 +0530

    Update DB.py

--- DB.py
+++ DB.py
@@ -1,102 +1,114 @@
-import requests
-import json
-
-def searchDb(collection,sort={},filter={},skip=None,limit=None):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/find"
-    payload = json.dumps({
-        "collection": collection,
-        "database": "api",
-        "dataSource": "Cluster0",
-        "filter": filter,
-        "sort": sort,
-        "limit": limit,
-        "skip": skip,})
-    headers = {
-      'Content-Type': 'application/json',
-      'Access-Control-Request-Headers': '*',
-      'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    return requests.request("POST", url, headers=headers, data=payload).json().get('documents')
+from tmdb import tmdb
+from datetime import datetime
+from pytz import timezone
+import sqlite3
 
 
-def updateDb(channel,Id,title):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/updateOne"
-    payload = json.dumps({
-        "collection": channel,
-        "database": "api",
-        "dataSource": "Cluster0",
-        "filter": {"Id": Id},
-        "update": {"$set": {"title": title}}
-    })
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    requests.request("POST", url, headers=headers, data=payload)
+def generateHash():
+    now = datetime.now(timezone("Asia/Kolkata"))
+    hash_value = now.strftime("%Y%m%d%H%M%S%f")
+    return hex(int(hash_value))[2:]
 
 
-def deleteDb(channel,Id):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/deleteOne"
-    payload = json.dumps({
-        "collection": channel,
-        "database": "api",
-        "dataSource": "Cluster0",
-        "filter": {"Id": Id},
-    })
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    requests.request("POST", url, headers=headers, data=payload)
+def connect():
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS MOVIES (id, title)")
+    cur.execute("CREATE TABLE IF NOT EXISTS SERIES (id, title)")
+    cur.execute("CREATE TABLE IF NOT EXISTS DRAMAS (id, title)")
+    cur.execute("CREATE TABLE IF NOT EXISTS ANIME (id, title)")
+    cur.execute("CREATE TABLE IF NOT EXISTS TMDB (id, title, genres, poster, date, cast, rating, type, overview)")
+    conn.commit()
+    conn.close()
 
 
-def addDb(document):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/insertOne"
-    payload = json.dumps({
-        "collection": "TMDB",
-        "database": "api",
-        "dataSource": "Cluster0",
-        "document":document
-    })
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    requests.request("POST", url, headers=headers, data=payload)
-
-def indexDb(filter={},skip=None,limit=None):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/find"
-    payload = json.dumps({
-        "collection": "TMDB",
-        "database": "api",
-        "dataSource": "Cluster0",
-        "filter": filter,
-        "sort": {"Id": -1},
-        "limit": limit,
-        "skip": skip, })
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    return requests.request("POST", url, headers=headers, data=payload).json().get('documents')
+def insertToTMDB(item):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    id = generateHash()
+    cur.execute("INSERT INTO TMDB VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",[id, item.get("title"), item.get("genres"), item.get("poster"), str(item.get("date")), item.get("cast"), item.get("rating"), item.get("type"), item.get("overview")])
+    conn.commit()
+    conn.close()
 
 
-def postDb(Id):
-    url = "https://ap-south-1.aws.data.mongodb-api.com/app/data-jktzb/endpoint/data/v1/action/findOne"
-    payload = {
-        "collection": "TMDB",
-        "database": "api",
-        "dataSource": "Cluster0",
-        "filter": {"Id":Id},
-        "projection": {"_id": 0, "Id": 0}
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'Access-Control-Request-Headers': '*',
-        'api-key': 'oEMu1rIsWSQgMm20dBo9av7uQ1FxIvtNgvR61QwjmcmqEuxAOyIGGl0VwS4QftiA',
-    }
-    return requests.request("POST", url, headers=headers, json=payload).json().get('document')
+def getIndex(limit=18, skip=0, search_term=None, order_by='Id', type=None):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    query = "SELECT id, title, type, poster FROM TMDB"
+    params = []
+    if search_term:
+        search_terms = search_term.split()
+        query += " WHERE " + " AND ".join(["title LIKE ?"] * len(search_terms))
+        params.extend(['%' + term + '%' for term in search_terms])
+    if type:
+        if params:
+            query += " AND"
+        else:
+            query += " WHERE"
+        query += " type = ?"
+        params.append(type)
+    query += f" ORDER BY {order_by} DESC LIMIT ? OFFSET ?"
+    params.extend([limit, skip])
+    cur.execute(query, params)
+    rows = cur.fetchall()
+    result = [{'Id': i[0], 'title': i[1], 'type': i[2], 'poster': i[3]} for i in rows]
+    cur.close()
+    conn.close()
+    return result
+
+
+def getFomTMDBByID(Id):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM TMDB WHERE id='{Id}'")
+    i = cur.fetchone()
+    return {'title':i[1],'genres':i[2],'poster':i[3],'date':i[4],'cast':i[5],"rating":i[6],"overview":i[8]}
+
+
+def getItems(table,search_term):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    query = f"SELECT id, title FROM {table.upper()}"
+    channel = 1 if table == "movies" else 2 if table == "series" else 3 if table == "dramas" else 4
+    params = []
+    if search_term:
+        search_terms = search_term.split()
+        query += " WHERE " + " AND ".join(["title LIKE ?"] * len(search_terms))
+        params.extend(['%' + term + '%' for term in search_terms])
+    query += f" ORDER BY title"
+    cur.execute(query, params)
+    rows = cur.fetchall()
+    result = [{'Id': i[0], 'title': i[1], 'channel':channel} for i in rows]
+    cur.close()
+    conn.close()
+    return result
+
+def addItems(table,id,title):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO {table.upper()} (?, ?)",[id, title])
+    conn.commit()
+    conn.close()
+
+
+def updateItems(table,id,title):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"UPDATE {table.upper()} title='{title}' WHERE id='{id}'")
+    conn.commit()
+    conn.close()
+
+
+def deleteItems(table,id):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"DELETE INTO {table.upper()} WHERE id='{id}'")
+    conn.commit()
+    conn.close()
+
+def updatePosts(id,title):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"UPDATE TMDB title='{title}' WHERE id='{id}'")
+    conn.commit()
+    conn.close()
