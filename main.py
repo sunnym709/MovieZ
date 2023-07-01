14dbe9a7edd0900ad31ff3f1b2e7eb0150da1746
sunnym709 <125447994+sunnym709@users.noreply.github.com>
2023-07-01 15:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask,request
 from flask import render_template
-from db import searchDb,updateDb, deleteDb, addDb, indexDb, postDb
-from tmdbv3api import TMDb
-from tmdbv3api import Movie,TV
+from DB import searchDb,updateDb, deleteDb, addDb, indexDb, postDb
+from tmdb import tmdb
 from datetime import datetime
 from pytz import timezone
+import sqlite3
 
 
 def generateHash():
@@ -15,9 +15,59 @@ def generateHash():
 
 
 app = Flask(__name__)
-tmdb = TMDb()
-tmdb.api_key = 'fe1a9cba269660db6066f930313e264b'
-app.secret_key = 'many random bytes'
+app.secret_key = 'fe1a9cba269660db6066f930313e264b'
+
+
+def connect():
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS TMDB (id, title, genres, poster, date, cast, rating, type, overview)")
+    conn.commit()
+    conn.close()
+connect()
+
+
+def insert(item):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    id = generateHash()
+    cur.execute("INSERT INTO TMDB VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",[id, item.get("title"), item.get("genres"), item.get("poster"), str(item.get("date")), item.get("cast"), item.get("rating"), item.get("type"), item.get("overview")])
+    conn.commit()
+    conn.close()
+
+
+def getHome(limit=18, skip=0, search_term=None, order_by='Id', type=None):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    query = "SELECT id, title, type, poster FROM TMDB"
+    params = []
+    if search_term:
+        search_terms = search_term.split()
+        query += " WHERE " + " AND ".join(["title LIKE ?"] * len(search_terms))
+        params.extend(['%' + term + '%' for term in search_terms])
+    if type:
+        if params:
+            query += " AND"
+        else:
+            query += " WHERE"
+        query += " type = ?"
+        params.append(type)
+    query += f" ORDER BY {order_by} DESC LIMIT ? OFFSET ?"
+    params.extend([limit, skip])
+    cur.execute(query, params)
+    rows = cur.fetchall()
+    result = [{'Id': i[0], 'title': i[1], 'type': i[2], 'poster': i[3]} for i in rows]
+    cur.close()
+    conn.close()
+    return result
+
+
+def getByTmdbID(Id):
+    conn = sqlite3.connect('api.db')
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM tmdb WHERE id='{Id}'")
+    i = cur.fetchone()
+    return {'title':i[1],'genres':i[2],'poster':i[3],'date':i[4],'cast':i[5],"rating":i[6],"overview":i[8]}
 
 
 @app.route('/admin/')
@@ -29,11 +79,9 @@ def admin():
 def home():
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('perPage', 18))
-    search_term = request.args.get('search', '')
-    pattern = '.*' + '.*'.join(search_term.strip().split(" ")) + '.*'
-    query = {"$and": [{"title": {"$regex": pattern, "$options": "i"}}]} if search_term != '' else {}
+    search_term = request.args.get('search', None)
     skip = (page - 1) * per_page
-    data = indexDb(filter=query, skip=skip, limit=per_page)
+    data = getHome(skip=skip,search_term=search_term)
     perv = None if page == 1 else page - 1
     next = None if len(data) != 18 else page + 1
     return render_template('index.html', data=data, perv=perv, next=next)
@@ -41,12 +89,14 @@ def home():
 
 @app.route('/post/<cat>/<id>')
 def post(cat,id):
-    post = postDb(id)
-    search = post.get('search')
-    search = ''.join(e for e in search if e.isalnum() or  e==" ")
+    post = getByTmdbID(id)
+    title = post.get("title")
+    date = post.get("date")
+    search = title + " " + date[:4] if cat == "movies" else title
+    search = ''.join(e for e in search if e.isalnum() or e == " ")
     pattern = '.*' + '.*'.join(search.strip().split(" ")) + '.*'
     query = {"$and": [{"title": {"$regex": pattern, "$options": "i"}}]}
-    data = searchDb(cat, sort={'title':1}, filter=query)
+    data = searchDb(cat, sort={'title': 1}, filter=query)
     return render_template('post.html',post=post,data=data)
 
 
@@ -54,118 +104,53 @@ def post(cat,id):
 def movies(cat):
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('perPage', 18))
-    search_term = request.args.get('search', '')
-    pattern = '.*' + '.*'.join(search_term.strip().split(" ")) + '.*'
-    query = {"$and": [{"title": {"$regex": pattern, "$options": "i"}}]} if search_term != '' else {}
+    search_term = request.args.get('search', None)
     skip = (page - 1) * per_page
-    data = indexDb(filter=query, skip=skip, limit=per_page)
+    data = getHome(skip=skip,search_term=search_term,type=cat)
     perv = None if page == 1 else page - 1
     next = None if len(data) != 18 else page + 1
     return render_template('index.html', data=data, perv=perv, next=next)
 
 
-@app.route('/admin/<cat>/')
-def adminc(cat):
-    page = int(request.args.get('page', 1))
-    per_page = int(request.args.get('perPage', 40))
-    search_term = request.args.get('search', '')
-    sort = int(request.args.get('sort', 1))
-    sort = -1 if cat == "movies" else 1
-    pattern = '.*' + '.*'.join(search_term.strip().split(" ")) + '.*'
-    query = {"$and": [{"title": {"$regex": pattern, "$options": "i"}}]} if search_term != '' else {}
-    skip = (page - 1) * per_page
-    data = searchDb(cat, sort={'Id':sort}, filter=query, skip=skip, limit=per_page)
-    perv = None if page == 1 else page - 1
-    next = None if len(data) != 40 else page + 1
-    return render_template('manage.html', data=data, perv=perv, next=next,channel=cat)
-
-
-@app.route("/update/<channel>/<id>",methods=['POST'])
-def update(channel,id):
-    title = request.json.get('title')
-    updateDb(channel,int(id),title)
-    return {'title':title,'id':'b'+id}
-
-
-@app.route("/delete/<channel>/<id>",methods=['POST'])
-def delete(channel,id):
-    deleteDb(channel,int(id))
-    return "ok"
-
-
 @app.route("/add/movie",methods=['POST'])
 def addMovie():
-    tmdb = request.form.get('tmdb')
-    tmdb = tmdb[tmdb.rindex("/")+1:].split("-")[0]
-    movie = Movie()
-    Id = generateHash()
-    try:
-        res = movie.details(int(tmdb))
-        overview = res.overview
-        title = res.title + " ("+res.release_date[:4]+")"
-        search = title.replace('(','').replace(')','')
-        poster = "https://image.tmdb.org/t/p/w500"+res.poster_path
-        document = {'Id':Id,'title':title,'search':search,'type':'movies','overview':overview,'poster':poster}
-        addDb(document)
-    except:
-        pass
+    Id = request.form.get('tmdb')
+    Id = Id[Id.rindex("/")+1:].split("-")[0]
+    item = tmdb(Id)
+    insert(item)
     return render_template('admin.html')
 
 
 @app.route("/add/series",methods=['POST'])
 def addSeries():
-    tmdb = request.form.get('tmdb')
-    tmdb = tmdb[tmdb.rindex("/")+1:].split("-")[0]
-    tv = TV()
-    Id = generateHash()
-    try:
-        res = tv.details(int(tmdb))
-        overview = res.overview
-        title = res.name + " ("+res.first_air_date[:4]+")"
-        search = res.name
-        poster = "https://image.tmdb.org/t/p/w500"+res.poster_path
-        document = {'Id':Id,'title':title,'search':search,'type':'series','overview':overview,'poster':poster}
-        addDb(document)
-    except:
-        pass
+    Id = request.form.get('tmdb')
+    Id = Id[Id.rindex("/") + 1:].split("-")[0]
+    item = tmdb(Id,"series")
+    insert(item)
     return render_template('admin.html')
+
 
 @app.route("/add/drama",methods=['POST'])
 def addDramas():
-    tmdb = request.form.get('tmdb')
-    tmdb = tmdb[tmdb.rindex("/")+1:].split("-")[0]
-    tv = TV()
-    Id = generateHash()
-    try:
-        res = tv.details(int(tmdb))
-        overview = res.overview
-        title = res.name + " ("+res.first_air_date[:4]+")"
-        search = res.name
-        poster = "https://image.tmdb.org/t/p/w500"+res.poster_path
-        document = {'Id':Id,'title':title,'search':search,'type':'dramas','overview':overview,'poster':poster}
-        addDb(document)
-    except:
-        pass
+    Id = request.form.get('tmdb')
+    Id = Id[Id.rindex("/") + 1:].split("-")[0]
+    item = tmdb(Id,"dramas")
+    insert(item)
     return render_template('admin.html')
 
 
 @app.route("/add/anime",methods=['POST'])
 def addAnime():
-    tmdb = request.form.get('tmdb')
-    tmdb = tmdb[tmdb.rindex("/")+1:].split("-")[0]
-    tv = TV()
-    Id = generateHash()
-    try:
-        res = tv.details(int(tmdb))
-        overview = res.overview
-        title = res.name + " ("+res.first_air_date[:4]+")"
-        search = res.name
-        poster = "https://image.tmdb.org/t/p/w500"+res.poster_path
-        document = {'Id':Id,'title':title,'search':search,'type':'anime','overview':overview,'poster':poster}
-        addDb(document)
-    except:
-        pass
+    Id = request.form.get('tmdb')
+    Id = Id[Id.rindex("/") + 1:].split("-")[0]
+    item = tmdb(Id,"anime")
+    insert(item)
     return render_template('admin.html')
+
+
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
